058d156d644e5006a927cf0fc17900040c01e0b5
mananrathi23 <144107804+mananrathi23@users.noreply.github.com>
2023-09-10 16:30:13 +0530

    Create Q2.py

---
+++ Q2.py
@@ -0,0 +1,9 @@
+x=int (input("enter a number:")) 
+y=int (input("enter a number:")) 
+if x>0 and y>0: 
+  if y%x==0: 
+    print(" y is divisible by x:")  
+  if y%x!=0: 
+    print(" y is not divisible by x") 
+elif x<0 and y<0: 
+    print("invalid input")
